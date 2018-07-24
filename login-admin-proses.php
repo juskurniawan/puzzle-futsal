af2bf0168f390e0576987b8bef774d8de557cc19
juskurniawan <41629337+juskurniawan@users.noreply.github.com>
2018-07-24 19:58:52 +0700

    Add files via upload

---
+++ login-admin-proses.php
@@ -0,0 +1,60 @@
+<?php
+/*require("config.php");
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysql_query($query,$con) or die(mysqli_error());
+$num_row = mysql_num_rows($result);
+		$row=mysql_fetch_array($result);
+		if( $num_row >=1 ) {
+			
+			$_SESSION['username']=$row['username'];
+			$_SESSION['password']=$row['password'];
+			header("location:user/index.php");
+
+			
+		}
+		else{
+			header("location:index.php?page=member&cat=1&ref=failed");
+		}
+*/?>
+<?php
+include ("conn.php");
+date_default_timezone_set('Asia/Jakarta');
+
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+
+if (empty($username) && empty($password)) {
+	header('location:login-admin.php?error1');
+	break;
+} else if (empty($username)) {
+	header('location:login-admin.php?error=2');
+	break;
+} else if (empty($password)) {
+	header('location:login-admin.php?error=3');
+	break;
+}
+
+$q = mysql_query("select * from administrator where username='$username' and password='$password'");
+$row = mysql_fetch_array ($q);
+
+if (mysql_num_rows($q) == 1) {
+	$_SESSION['username'] = $username;
+	$_SESSION['password'] = $row['password'];
+
+	header('location:admin/index.php');
+} else {
+	header('location:login-admin.php?error=4');
+}
+?>
