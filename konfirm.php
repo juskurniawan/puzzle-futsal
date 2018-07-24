37f73535a024c66baf4a4e5b5da8bf7d8f35b777
juskurniawan <41629337+juskurniawan@users.noreply.github.com>
2018-07-25 02:38:23 +0700

    Delete konfirm.php

--- konfirm.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include "conn.php";
-	$email = $_GET['email']; 	$username = $_GET['username']; 
-	
-		$query = mysql_query("UPDATE users SET aktif = 'Y' WHERE email = '".$email."'") or die (mysql_error()); 	
-		if($query) { 		
-			echo "Member dengan username <strong>".$username."</strong> telah diaktifkan"; 	} 
-			else {
-			 		echo "Gagal diaktifkan"; 	} ?> 
